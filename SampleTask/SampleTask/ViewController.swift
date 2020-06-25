308491f4ecac89049353ce963dfb993efa502cf7
Piyush <piyushkumarjaiswal@gmail.com>
2020-06-25 11:51:11 +0530

    Initial Commit
    Initialize Project with required configuration

---
+++ SampleTask/SampleTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTask
+//
+//  Created by Piyush on 25/06/20.
+//  Copyright © 2020 Piyush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
